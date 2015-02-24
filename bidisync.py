61b89fa1a82524e06ac384f3eecc75d92d1a49d6
Davi R. Tavares <davirtavares@gmail.com>
2015-02-24 01:06:35 -0300

    Implementation of the daemon.

--- bidisync.py
+++ bidisync.py
@@ -1,36 +1,47 @@
 # -*- coding: UTF-8 -*-
 
-from datetime import datetime
-from uuid import uuid4
+import argparse
+from time import sleep
 
+from daemon import DaemonContext
 from cassandra.cluster import Cluster
 from elasticsearch import Elasticsearch
 
-import driver
 from util import *
 from cassandradriver import CassandraDriver
 from elasticsearchdriver import ElasticsearchDriver
 
+def run():
+    while True:
+        cas_cluster = Cluster(get_conf("CASSANDRA_CONTACT_POINTS"), \
+                get_conf("CASSANDRA_PORT"))
+        es_conn = Elasticsearch(get_conf("ELASTICSEARCH_NODES"))
+
+        cd = CassandraDriver(cas_cluster)
+        ed = ElasticsearchDriver(es_conn, \
+                get_conf("ELASTICSEARCH_INDEX_NAME"), \
+                get_conf("ELASTICSEARCH_DOC_TYPE"))
+
+        es_delta = cd.build_delta(ed)
+        es.apply()
+
+        cas_delta = ed.build_delta(cd)
+        cas_delta.apply()
+
+        sleep(get_conf("SYNC_POLLING_INTERVAL"))
+
 if __name__ == "__main__":
-    cas_cluster = Cluster(get_conf("CASSANDRA_CONTACT_POINTS"), \
-            get_conf("CASSANDRA_PORT"))
-    es_conn = Elasticsearch(get_conf("ELASTICSEARCH_NODES"))
+    parser = argparse.ArgumentParser()
 
-    cd = CassandraDriver(cas_cluster)
-    ed = ElasticsearchDriver(es_conn, get_conf("ELASTICSEARCH_INDEX_NAME"), \
-            get_conf("ELASTICSEARCH_DOC_TYPE"))
+    parser.add_argument("--foreground", "-f", dest="foreground", \
+            help="run in foreground", action="store_true")
+    parser.set_defaults(foreground=False)
 
-    qs = cd.get_queryset()
-    a = next(qs)
+    args = parser.parse_args()
 
-    a["last_modified"] = datetime.utcnow()
+    if not args.foreground:
+        with DaemonContext():
+            run()
 
-    op = driver.Operation(driver.Operation.OPER_UPDATE, a)
-
-    cd.execute_operation(op)
-
-    import pdb; pdb.set_trace()
-
-    delta = cd.build_delta(ed)
-
-    import pdb; pdb.set_trace()
+    else:
+        run()
